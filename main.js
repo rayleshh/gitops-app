46f7bd7d0a03411d5db381a53a09d1e1a7592c10
Melquisedeque Bernardes Pereira <10271992+rayleshh@users.noreply.github.com>
2021-09-23 13:38:35 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,28 @@
 const http = require('http');
 
+// Logger
+const { createLogger, format, transports } = require('winston');
+
+const logger = createLogger({
+  level: 'info',
+  exitOnError: false,
+  format: format.json(),
+  transports: [
+    new transports.File({ filename: `./logs/app.log` }),
+  ],
+});
+
+module.exports = logger;
+
+// Datadog
+const tracer = require('dd-trace').init({
+  logInjection: true
+});
+
+// APP
 const hostname = '0.0.0.0';
 const port = 8080;
+
 
 console.log(process.env.ENVIRONMENT || 'e2e');
 
@@ -12,7 +33,8 @@ console.log(process.env.ENVIRONMENT || 'e2e');
 const server = http.createServer((_, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, Vitão!\n');
+  res.end('Hello, Vitao!\n');
+  logger.info('Get API: ' + Math.random() ,{color: 'blue' });
 });
 
 
